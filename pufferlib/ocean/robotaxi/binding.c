d477c50acc966dcaa6a05fce3f238b27cecad952
Spencer Cheng <spenccheng@gmail.com>
2025-05-16 12:52:02 +0000

    first commit  with nuplan map loaded - stripped gpudrive

---
+++ pufferlib/ocean/robotaxi/binding.c
@@ -0,0 +1,105 @@
+#include "robotaxi.h"
+#define Env Robotaxi
+#define MY_SHARED
+#include "../env_binding.h"
+
+static PyObject* my_shared(PyObject* self, PyObject* args, PyObject* kwargs) {
+    int num_agents = unpack(kwargs, "num_agents");
+    int num_maps = unpack(kwargs, "num_maps");
+    // GPUDrive* temp_envs = calloc(num_envs, sizeof(GPUDrive));
+    // PyObject* agent_offsets = PyList_New(num_envs+1);
+    // PyObject* map_ids = PyList_New(num_envs);
+    srand(time(NULL));
+    int total_agent_count = 0;
+    int env_count = 0;
+    int max_envs = num_agents;
+    PyObject* agent_offsets = PyList_New(max_envs+1);
+    PyObject* map_ids = PyList_New(max_envs);
+    // getting env count
+    while(total_agent_count < num_agents && env_count < max_envs){
+        char map_file[100];
+        int map_id = rand() % num_maps;
+        GPUDrive* env = calloc(1, sizeof(GPUDrive));
+        sprintf(map_file, "resources/gpudrive/binaries/map_%03d.bin", map_id);
+        env->entities = load_map_binary(map_file, env);
+        set_active_agents(env);
+        // Store map_id
+        PyObject* map_id_obj = PyLong_FromLong(map_id);
+        PyList_SetItem(map_ids, env_count, map_id_obj);
+        // Store agent offset
+        PyObject* offset = PyLong_FromLong(total_agent_count);
+        PyList_SetItem(agent_offsets, env_count, offset);
+        total_agent_count += env->active_agent_count;
+        env_count++;
+        for(int j=0;j<env->num_entities;j++) {
+            free_entity(&env->entities[j]);
+        }
+        free(env->entities);
+        free(env->active_agent_indices);
+        free(env->static_car_indices);
+        free(env->expert_static_car_indices);
+        free(env);
+    }
+    if(total_agent_count >= num_agents){
+        total_agent_count = num_agents;
+    }
+    PyObject* final_total_agent_count = PyLong_FromLong(total_agent_count);
+    PyList_SetItem(agent_offsets, env_count, final_total_agent_count);
+    PyObject* final_env_count = PyLong_FromLong(env_count);
+    // resize lists
+    PyObject* resized_agent_offsets = PyList_GetSlice(agent_offsets, 0, env_count + 1);
+    PyObject* resized_map_ids = PyList_GetSlice(map_ids, 0, env_count);
+    //
+    Py_DECREF(agent_offsets);
+    Py_DECREF(map_ids);
+    // create a tuple
+    PyObject* tuple = PyTuple_New(3);
+    PyTuple_SetItem(tuple, 0, resized_agent_offsets);
+    PyTuple_SetItem(tuple, 1, resized_map_ids);
+    PyTuple_SetItem(tuple, 2, final_env_count);
+    return tuple;
+
+    //Py_DECREF(num);
+    /*
+    for(int i = 0;i<num_envs; i++) {
+        for(int j=0;j<temp_envs[i].num_entities;j++) {
+            free_entity(&temp_envs[i].entities[j]);
+        }
+        free(temp_envs[i].entities);
+        free(temp_envs[i].active_agent_indices);
+        free(temp_envs[i].static_car_indices);
+    }
+    free(temp_envs);
+    */
+    // return agent_offsets;
+}
+
+static int my_init(Env* env, PyObject* args, PyObject* kwargs) {
+    env->human_agent_idx = unpack(kwargs, "human_agent_idx");
+    env->reward_vehicle_collision = unpack(kwargs, "reward_vehicle_collision");
+    env->reward_offroad_collision = unpack(kwargs, "reward_offroad_collision");
+    env->spawn_immunity_timer = unpack(kwargs, "spawn_immunity_timer");
+    int map_id = unpack(kwargs, "map_id");
+    int max_agents = unpack(kwargs, "max_agents");
+
+    char map_file[100];
+    sprintf(map_file, "resources/gpudrive/binaries/map_%03d.bin", map_id);
+    env->map_name = map_file;
+    env->num_agents = max_agents;
+    init(env);
+    return 0;
+}
+
+static int my_log(PyObject* dict, Log* log) {
+    assign_to_dict(dict, "perf", log->perf);
+    assign_to_dict(dict, "score", log->score);
+    assign_to_dict(dict, "episode_return", log->episode_return);
+    assign_to_dict(dict, "episode_length", log->episode_length);
+    assign_to_dict(dict, "offroad_rate", log->offroad_rate);
+    assign_to_dict(dict, "collision_rate", log->collision_rate);
+    assign_to_dict(dict, "dnf_rate", log->dnf_rate);
+    assign_to_dict(dict, "n", log->n);
+    assign_to_dict(dict, "completion_rate", log->completion_rate);
+    assign_to_dict(dict, "clean_collision_rate", log->clean_collision_rate);
+    return 0;
+}
